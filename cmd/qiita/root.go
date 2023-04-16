7b793cdc49b279b78aec463963e35ab691def9ce
koki-develop <kou.pg.0131@gmail.com>
2023-04-16 15:10:30 +0900

    remove main.go

--- cmd/qiita/root.go
+++ cmd/qiita/root.go
@@ -1,12 +1,22 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:          "qiita",
 	Short:        "CLI for Qiita",
 	Long:         "CLI for Qiita",
 	SilenceUsage: true,
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
 
 func init() {
