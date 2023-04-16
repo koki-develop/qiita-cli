7fc62f9508bdd5e7dc9a4d6635eb76561cf6c635
koki-develop <kou.pg.0131@gmail.com>
2023-04-16 18:39:33 +0900

    use json.Encoder

--- internal/printer/json.go
+++ internal/printer/json.go
@@ -15,16 +15,10 @@ func NewJSONPrinter() *JSONPrinter {
 	return &JSONPrinter{}
 }
 
-func (*JSONPrinter) Print(w io.Writer, p Printable) error {
-	j, err := json.MarshalIndent(p, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	if _, err := w.Write(j); err != nil {
-		return err
-	}
-	if _, err := w.Write([]byte{'\n'}); err != nil {
+func (*JSONPrinter) Print(w io.Writer, cols []string, p Printable) error {
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(p); err != nil {
 		return err
 	}
 	return nil
