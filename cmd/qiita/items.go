bcd4c6a5164204ab3e6aaf2db7901be7b787b77f
koki-develop <kou.pg.0131@gmail.com>
2023-04-16 15:09:00 +0900

    add items list subcommand

--- cmd/qiita/items.go
+++ cmd/qiita/items.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+)
 
 var itemsCmd = &cobra.Command{
 	Use:   "items",
 	Short: "Manage items",
 	Long:  "Manage items.",
+}
+
+var itemsListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list items",
+	Long:  "list items.",
+	Args:  cobra.ExactArgs(0),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
 }
--- cmd/qiita/root.go
+++ cmd/qiita/root.go
@@ -3,13 +3,18 @@ package main
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use:   "qiita",
-	Short: "CLI for Qiita",
-	Long:  "CLI for Qiita",
+	Use:          "qiita",
+	Short:        "CLI for Qiita",
+	Long:         "CLI for Qiita",
+	SilenceUsage: true,
 }
 
 func init() {
 	rootCmd.AddCommand(
 		itemsCmd,
+	)
+
+	itemsCmd.AddCommand(
+		itemsListCmd,
 	)
 }
