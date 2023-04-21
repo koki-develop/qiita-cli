0bba2e4d9c4a9be45b72f91a354176caabf4775a
koki-develop <kou.pg.0131@gmail.com>
2023-04-21 12:07:11 +0900

    configure コマンドの short, long, args を設定

--- cmd/qiita/configure.go
+++ cmd/qiita/configure.go
@@ -6,7 +6,10 @@ import (
 )
 
 var configureCmd = &cobra.Command{
-	Use: "configure",
+	Use:   "configure",
+	Short: "Configure Qiita CLI",
+	Long:  "Configure Qiita CLI.",
+	Args:  cobra.ExactArgs(0),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := &config.Config{
 			AccessToken: *flagConfigureAccessToken.Get(cmd, true),
